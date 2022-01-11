9403019c734786d31e4f3abb1042e30b04d09e9a
Berkin Polat <brknpolat@gmail.com>
2022-01-11 15:45:08 +0300

    Update Student.py

--- pythonProject/Student.py
+++ pythonProject/Student.py
@@ -1,21 +1,24 @@
+from StudentID import StudentID
 class Student:
     def __init__(self, *args):
 
-        if len(args) == 6:
+        if len(args) == 5:
             self.firstName = args[0]
             self.lastName = args[1]
+
             self.studentID = args[2]
             self.transcript = args[3]
             self.advisor = args[4]
             self.semester = 1
-            self.completedCredits = args[6]
+            self.completedCredits = 0
             self.courseList = []
 
         else:
             #firstName, lastName, studentID, transcript, advisor, semester, completedCredits
             self.firstName = args[0]
             self.lastName = args[1]
-            self.studentID = args[2]
+            studentID = StudentID(args[2])
+            self.studentID = studentID
             self.transcript = args[3]
             self.advisor = args[4]
             self.semester = args[5]
@@ -29,3 +32,9 @@ class Student:
         for i in self.courseList:
             if i.courseID == course.courseID:
                 self.courseList.remove(i)
+
+    """
+    select course function(self):
+        get courses according to their semester with student's semester.
+        
+    """
