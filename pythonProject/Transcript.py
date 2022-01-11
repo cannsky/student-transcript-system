1dfc0ff2528394bbf20376ac069976ad0a80e5f8
cgok <44663880+cgok@users.noreply.github.com>
2022-01-11 16:26:28 +0300

    Added Transcript fix, Json implementation for transcript
    This commit provides some random transcript creation fixes and also this commit enables simulation to save all of the features of a student to the json files and reading them is also available from now on.

--- pythonProject/Transcript.py
+++ pythonProject/Transcript.py
@@ -1,54 +1,123 @@
+from decimal import Decimal
+
+#################### düzenlenmiş transcript
 class Transcript:
-    courseList = []
-    numberGradeList = []
-    letterGradeList = []
+    letterGradeDict = {
+        "AA": 4.00,
+        "BA": 3.50,
+        "BB": 3.00,
+        "BC": 2.50,
+        "CC": 2.00,
+        "DC": 1.50,
+        "DD": 1.00,
+        "FD": 0.50,
+        "FF": 0.00,
+    }
 
-    creditTakenS = None
-    creditTakenY = None
-    creditCompletedS = None
-    creditCompletedY = None
-    scoreS = None
-    scoreY = None
-    gpa = None
-    cgpa = None
+    # transcriptList = [
+    #       [semester, [taken course objects list], creditTaken , creditCompleted , scoreSemester  , gpa , cgpa ],
+    #       [semester, [taken course objects list], creditTaken , creditCompleted , scoreSemester , gpa , cgpa ],
+    #                                              ....
+    #                                              ....
+    #            ] Note = taken course objects list be like = [course obj , lettergrade , taken semester]
 
-    def __init__(self, transcriptJsonList, transCriptJsonStudentInfo, type):
-        if(type == 0): return
-        self.transcriptJsonList = transcriptJsonList
-        self.fillLists(self.courseList, self.numberGradeList, self.letterGradeList, self.transcriptJsonList)
+    # transcriptTemplate = [ [course , letter grade , taken semester ],
+    #                        [course , letter grade , taken semester ],                                        ,
+    #                                        ....                    ]
 
+    def __init__(self, transcriptTemplate, transCriptJsonStudentInfo):
         self.ID = transCriptJsonStudentInfo[0]
         self.fName = transCriptJsonStudentInfo[1]
         self.lName = transCriptJsonStudentInfo[2]
         self.currentSemester = transCriptJsonStudentInfo[3]
+        self.transcriptList = self.calcSemestersDetails(transcriptTemplate, self.currentSemester)
 
-    def fillLists(self, courseList, numberGradeList, letterGradeList, transcriptJsonList):
+    # list = [ [semester, [taken course objects], creditTaken , creditCompleted , scoreSemester , gpa , cgpa ],
+    def calcSemestersDetails(self, transcriptTemplate, currentSemester):
+        tempTranscriptList = []
+        tListTemplate = []
+        currSemesterCourses = []
+        sem = 1
+        while sem < currentSemester:
+            tListTemplate.insert(0, sem)
+            for i in transcriptTemplate:
+                if i[2] == str(sem):
+                    currSemesterCourses.append(i)
+            tListTemplate.insert(1, currSemesterCourses)
+            creditTaken = self.calcCreditTaken(currSemesterCourses)
+            tListTemplate.insert(2, creditTaken)
+            tListTemplate.insert(3, self.calcCreditCompleted(currSemesterCourses))
+            score = self.calcScoreSemester(currSemesterCourses)
+            tListTemplate.insert(4, score)
+            gpa = (self.calcGPA(score, creditTaken))
+            gpaStr = str(gpa)[:4]
+            gpa = Decimal(gpaStr)
+            tListTemplate.insert(5, gpa)
+            tListTemplate.insert(6, None)
+            tempTranscriptList.append(tListTemplate)
 
-        for i in transcriptJsonList:
-            letterGradeList.append(i[1])
-            numberGradeList.append(i[2])
-            # there is something on there.
+            currSemesterCourses = []
+            tListTemplate = []
+            sem += 1
+        self.calcCGPA(currentSemester, tempTranscriptList)
+        return tempTranscriptList
 
-    def calcCreditTakenS(self):
-        print("Credits that taken for current semestre")
+    def calcCreditTaken(self, tempList):
+        credit = 0
+        for i in tempList:
+            credit += int(i[0].credit[0])
+        return credit
 
-    def calcCreditTakenY(self):
-        print("Credits that taken for whole year")
+    def calcCreditCompleted(self, tempList):
+        credit = 0
+        for i in tempList:
+            if i[1] != "FF":
+                credit += int(i[0].credit[0])
+        return credit
 
-    def calcCreditCompletedS(self):
-        print("Credits that completed succesfuly for current semestre")
+    def calcScoreSemester(self, tempList):
+        score = 0
+        for i in tempList:
+            i1 = Decimal(self.letterGradeDict[i[1]])
+            i2 = Decimal(i[0].credit[0])
+            i3 = i1 * i2
+            score += i3
+        return score
 
-    def calcCreditCompletedY(self):
-        print("Credits that completed succesfuly for whole year")
+    def calcGPA(self, score, creditTaken):
+        return score / creditTaken
 
-    def calcScoreS(self):
-        print("Score for current semestre")
+    def calcCGPA(self, currentSemester, tempTranscriptList):
+        n = 1
+        cgpa = 0
+        while n < currentSemester:
+            for i in tempTranscriptList:
+                if n >= i[0]:
+                    gpa_s = str(i[5])
+                    gpa = Decimal(gpa_s)
+                    cgpa += gpa
+            cgpa = cgpa / n
+            cgpa_s = str(cgpa)[:4]
+            cgpa = Decimal(cgpa_s)
+            tempTranscriptList[n - 1].insert(6, cgpa)
+            cgpa = 0
+            n += 1
 
-    def calcScoreY(self):
-        print("Score for whole year")
+    def show(self):
+        for i in self.transcriptList:
+            print("*******************************")
+            print("Semester : ", i[0])
+            print("*******************************")
+            for j in i[1]:
+                print("Course :", j[0].courseCode, j[0].courseName,
+                      "\nCourse Semester:", j[0].semester, "\nTaken Semester:", j[2],
+                      "\nCredit:", j[0].credit[0], "\nLetter Grade:", j[1])
+                print("........")
 
-    def calcGPA(self):
-        print("Grade Point Average for semestre")
+            print("Credit Taken = ", i[2])
+            print("Credit Completed = ", i[3])
+            print("Score Semester = ", i[4])
+            print("GPA = ", i[5])
+            print("CGPA = ", i[6])
 
-    def calcCGPA(self):
-        print("Cumulative Grade Point Avarage for whole year")
+############################################### transcripti oluşturan random method
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -22,7 +22,8 @@ class SystemTests:
         student_affairs = StudentAffairs()
         random_students = student_affairs.create_random_student_list(100, 2018)
         for student in random_students:
-            print(student.firstName + " " + student.lastName + " " + student.studentID.fullID + " " + "Completed Credits: " + student.completedCredits)
+            print(student.firstName + " " + student.lastName + " " + student.studentID.fullID + " " + "Completed Credits: " + str(student.completedCredits))
+            StudentAffairs.write_json(JsonSettings(JsonType.STUDENT, student.studentID.fullID), student)
 
 
 class JsonType(Enum):
@@ -47,14 +48,17 @@ class StudentAffairs:
         self.firstNameList = file1.readlines()
         file1 = open('lastname.txt', 'r', encoding='UTF-8')
         self.lastNameList = file1.readlines()
+        self.courses = StudentAffairs.read_json(JsonSettings(JsonType.COURSE, None))
 
     def create_random_student_list(self, count, year):
         students = []
         for i in range(count):
             student_id = StudentID(year - int(0), i + 1)
-            student = Student(random.choice(self.firstNameList).strip('\n'), random.choice(self.lastNameList).strip('\n'),
+            first_name = random.choice(self.firstNameList).strip('\n')
+            second_name = random.choice(self.lastNameList).strip('\n')
+            student = Student(first_name, second_name,
                               student_id,
-                              Transcript([],[], 0), "advisor", "semester", "schedule", 0)
+                              self.randomTranscript(self.courses, [student_id, first_name, second_name, 4]), "advisor", 4, "schedule", 0)
             students.append(student)
         return students
 
@@ -72,31 +76,48 @@ class StudentAffairs:
                 "Name": obj.firstName,
                 "Surname": obj.lastName,
                 "StudentID": obj.studentID.fullID,
+                "Semester": obj.semester,
                 "AdvisorID": 150118000,
                 "Completed Credits": obj.completedCredits,
                 "Transcript": []
             }
-            for i in range(len(obj.transcript.courseList)):
-                obj_dict["Transcript"].append({
-                    obj.transcript.courseList[i]: obj.transcript.letterGradeList[i]
-                })
-                obj_dict["Completed Credits"] += obj.transcript.courseList[i].credit
+            for i in range(len(obj.transcript.transcriptList)):
+                for j in range(len(obj.transcript.transcriptList[i])):
+                    for k in range(len(obj.transcript.transcriptList[i][1])):
+                        obj_dict['Transcript'].append([
+                            obj.transcript.transcriptList[i][1][k][0].courseCode,
+                            obj.transcript.transcriptList[i][1][k][1],
+                            obj.transcript.transcriptList[i][1][k][2],
+                        ])
         return obj_dict
 
     @staticmethod
     def get_obj(json_type, data_dict):
         if json_type == JsonType.STUDENT:
-            obj = Student(data_dict.name, data_dict.surname)
+            obj = Student(data_dict["Name"],
+                          data_dict["Surname"],
+                          data_dict["StudentID"],
+                          Transcript(
+                              data_dict["Transcript"],
+                              [data_dict["StudentID"],
+                               data_dict["Name"],
+                               data_dict["Surname"],
+                               data_dict["Semester"]]
+                          ),
+                          data_dict["AdvisorID"],
+                          data_dict["Semester"],
+                          "schedule",
+                          data_dict["Completed Credits"])
         else:
             obj = []
-            prerequisites = []
+            prerequisites = None
             a = 0
             for data in data_dict:
                 if data["Prerequsite"] is not None:
                     for i in range(len(obj)):
                         for j in range(len(data["Prerequsite"])):
                             if obj[i].courseCode == data["Prerequsite"][j]:
-                                prerequisites.append(obj[i])
+                                prerequisites = obj[i]
                 obj.append(Course(data["Lecture Name"],
                            data["Lecture Code"],
                            data["Lecture Type"],
@@ -107,7 +128,7 @@ class StudentAffairs:
                            5,
                            None,
                            None))
-                prerequisites = []
+                prerequisites = None
         return obj
 
     @staticmethod
@@ -125,7 +146,187 @@ class StudentAffairs:
 
         return StudentAffairs.get_obj(json_settings.json_type, data)
 
+    def calculateTotalCredit(self, transcriptTemplate, semesterCounter):
+        totalCredit = 0
+        for i in transcriptTemplate:
+            if i[2] == str(semesterCounter):
+                if i[1] != "FF":
+                    totalCredit += int(i[0].credit[0])
+        return totalCredit
+
+    def createFailedCourseList(self, transcriptTemplate, currentSemester):
+        failedCourseList = []
+        num = 1
+        while num < currentSemester:
+            for i in transcriptTemplate:
+                if str(num) == i[0].semester:
+                    if i[1] == "FF":
+                        failedCourseList.append(i)
+            num += 1
+        return failedCourseList
+
+    def assignCourse(self, transcriptTemplate, currentSemester, letterGradeList):
+        retL = []
+        failedCourseList = []
+        for i in transcriptTemplate:
+            if i[1] == "FF":
+                failedCourseList.append(i)
+
+        semInfo = []
+        n = 1
+        while n < currentSemester:
+            l = []
+            l.insert(0, n)
+            l.insert(1, self.calculateTotalCredit(transcriptTemplate, n))
+            semInfo.append(l)
+            n += 1
+        l = []
+        for t in semInfo:
+
+            if int(t[1]) < 30 and int(t[0]) > 1:
+                for j in failedCourseList:
+                    if int(j[2]) < int(t[0]):
+                        print("...", j[0].credit[0], j[0].courseCode, j[2], t[0])
+                        tmp = []
+                        tmp.insert(0, j[0])
+                        tmp.insert(1, random.choice(letterGradeList))
+                        tmp.insert(2, str(t[0]))
+                        transcriptTemplate.append(tmp)
+                        failedCourseList.remove(j)
+
+    def randomTranscript(self, wholeCourseList, transCriptJsonStudentInfo):
+        letterGradeList = ["AA", "BA", "BB", "BC", "CC", "DC", "DD", "FD", "FF"]
+        currentSemester = transCriptJsonStudentInfo[3]
+        semesterLimit = currentSemester - 1
+        courseNTEandUE = []
+        courseFTE = []
+        courseTE = []
+        transcriptTemplate = []
+        totalCredit = 0
+        for i in wholeCourseList:
+            if i.semester == "11":
+                courseNTEandUE.append(i)
+            if i.semester == "10":
+                courseFTE.append(i)
+            if i.semester == "9":
+                courseTE.append(i)
+
+        totalCredit = 0
+        tempList = []
+        semesterCounter = 1
+        while (semesterCounter <= semesterLimit):
+
+            if semesterCounter == 1:
+                for i in wholeCourseList:
+                    if str(semesterCounter) == i.semester:
+                        tempList.insert(0, i)
+                        tempList.insert(1, random.choice(letterGradeList))
+                        tempList.insert(2, i.semester)
+                        transcriptTemplate.append(tempList)
+                        tempList = []
+
+            elif semesterCounter > 1:
+                for i in wholeCourseList:
+                    if str(semesterCounter) == i.semester:
+
+                        if i.courseType == "Mandatory":
+                            if i.prerequisites is None:
+                                tempList.insert(0, i)
+                                tempList.insert(1, random.choice(letterGradeList))
+                                tempList.insert(2, i.semester)
+                                transcriptTemplate.append(tempList)
+                                tempList = []
+                            else:
+                                for j in transcriptTemplate:
+                                    if j[0].courseCode == i.prerequisites.courseCode and j[1] != "FF":
+                                        tempList.insert(0, i)
+                                        tempList.insert(1, random.choice(letterGradeList))
+                                        tempList.insert(2, i.semester)
+                                        transcriptTemplate.append(tempList)
+                                        tempList = []
+
+                        else:
+                            if i.courseCode.split("x", 1)[0] == "NTE" or i.courseCode.split("x", 1)[0] == "UE":
+                                randomListNTEandUE = []
+                                for a in courseNTEandUE:
+                                    if (int(a.courseCode[-4]) == int(semesterCounter / 2 + 1)):
+                                        randomListNTEandUE.append(a)
+                                randomCourse = random.choice(randomListNTEandUE)
+                                if randomCourse.prerequisites is None:
+                                    tempList.insert(0, randomCourse)
+                                    tempList.insert(1, random.choice(letterGradeList))
+                                    tempList.insert(2, randomCourse.semester)
+                                    transcriptTemplate.append(tempList)
+
+                                    tempList = []
+                                    randomListNTEandUE = []
+                                else:
+
+                                    for j in transcriptTemplate:
+                                        if (j[0].courseCode == randomCourse.prerequisites.courseCode and j[1] != "FF"):
+                                            tempList.insert(0, randomCourse)
+                                            tempList.insert(1, random.choice(letterGradeList))
+                                            tempList.insert(2, randomCourse.semester)
+                                            transcriptTemplate.append(tempList)
+                                            tempList = []
+                                            randomListNTEandUE = []
+
+                            elif i.courseCode.split("x", 1)[0] == "FTE":
+                                randomListFTE = []
+                                for a in courseFTE:
+                                    if (int(a.courseCode[-4]) == int(semesterCounter / 2 + 1)):
+                                        randomListFTE.append(a)
+                                randomCourse = random.choice(randomListFTE)
+
+                                if randomCourse.prerequisites is None:
+                                    tempList.insert(0, randomCourse)
+                                    tempList.insert(1, random.choice(letterGradeList))
+                                    tempList.insert(2, randomCourse.semester)
+                                    transcriptTemplate.append(tempList)
+                                    tempList = []
+                                    randomListFTE = []
+
+                                else:
+
+                                    for j in transcriptTemplate:
+                                        if (j[0].courseCode == randomCourse.prerequisites.courseCode and j[1] != "FF"):
+                                            tempList.insert(0, randomCourse)
+                                            tempList.insert(1, random.choice(letterGradeList))
+                                            tempList.insert(2, randomCourse.semester)
+                                            transcriptTemplate.append(tempList)
+                                            tempList = []
+                                            randomListFTE = []
+                            elif i.courseCode.split("x", 1)[0] == "TE":
+                                randomListTE = []
+                                for a in courseTE:
+                                    if (int(a.courseCode[-4]) == int(semesterCounter / 2 + 1)):
+                                        randomListTE.append(a)
+                                randomCourse = random.choice(randomListTE)
+
+                                if randomCourse.prerequisites is None:
+                                    tempList.insert(0, randomCourse)
+                                    tempList.insert(1, random.choice(letterGradeList))
+                                    tempList.insert(2, randomCourse.semester)
+                                    transcriptTemplate.append(tempList)
+                                    tempList = []
+                                    randomListTE = []
+                                else:
+
+                                    for j in transcriptTemplate:
+                                        if (j[0].courseCode == randomCourse.prerequisites.courseCode and j[1] != "FF"):
+                                            tempList.insert(0, randomCourse)
+                                            tempList.insert(1, random.choice(letterGradeList))
+                                            tempList.insert(2, randomCourse.semester)
+                                            transcriptTemplate.append(tempList)
+                                            tempList = []
+                                            randomListTE = []
+
+            semesterCounter += 1
+
+        self.assignCourse(transcriptTemplate, currentSemester, letterGradeList)
+        t = Transcript(transcriptTemplate, transCriptJsonStudentInfo)
+        return t
 
 # StudentAffairs.save_json(x["Name"], x["Surname"])
 
-SystemTests.test_course_prerequisites()
+SystemTests.test_random_student_creation()
