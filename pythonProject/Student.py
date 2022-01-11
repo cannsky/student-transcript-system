319e4f84643c9b3e90c388a7f582ec295c5a2897
Berkin Polat <brknpolat@gmail.com>
2022-01-11 10:33:43 +0300

    Update Student.py

--- pythonProject/Student.py
+++ pythonProject/Student.py
@@ -1,14 +1,26 @@
 class Student:
-    def __init__(self, firstName, lastName, studentID, transcript, advisor, semester, schedule, completedCredits):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.studentID = studentID
-        self.transcript = transcript
-        self.advisor = advisor
-        self.semester = semester
-        self.schedule = schedule
-        self.completedCredits = completedCredits
-        self.courseList = []
+    def __init__(self, *args):
+
+        if len(args == 8):
+            self.firstName = args[0]
+            self.lastName = args[1]
+            self.studentID = args[2]
+            self.transcript = args[3]
+            self.advisor = args[4]
+            self.semester = 1
+            self.completedCredits = args[6]
+            self.courseList = []
+
+        else:
+            #firstName, lastName, studentID, transcript, advisor, semester, completedCredits
+            self.firstName = args[0]
+            self.lastName = args[1]
+            self.studentID = args[2]
+            self.transcript = args[3]
+            self.advisor = args[4]
+            self.semester = args[5]
+            self.completedCredits = args[6]
+            self.courseList = []
 
     def addToCourses(self, course):
         self.courseList.append(course)
